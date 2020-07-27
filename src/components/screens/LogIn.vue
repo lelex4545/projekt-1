45fd356d3da5611e4807ab9950791d4b325453a5
Selim <elasseli@googlemail.com>
2020-07-27 13:09:31 +0200

    Login_Alpha_v_0.041

--- src/components/screens/LogIn.vue
+++ src/components/screens/LogIn.vue
@@ -2,8 +2,8 @@
   <div id="main">
     <div id="container" class="conDesign">
       <p id="anmText">Anmelden</p>
-      <p id = "p1">Name</p>
-      <p id = "p2">Passwort</p>
+      <p id="p1">Name</p>
+      <p id="p2">Passwort</p>
       <form id="form" method="get">
         <input type="text" id="acc" name="acc" />
         <input type="password" id="pw" name="pw" />
@@ -20,45 +20,50 @@
     <div id="container2" class="conDesign">
     </div>
     -->
-    <br>
-    <span v-if="accScope"><Regist/></span>
-    <span v-else-if="pwScope"></span>
-    <span v-else></span>
+    <br class="canttouchme" />
+    <span v-if="accScope">
+      <Regist />
+    </span>
+    <span v-if="pwScope">
+      <PwForgot />
+    </span>
   </div>
 </template>
 
 <script>
 import Button from "./../Button";
 import Regist from "./Regist";
+import PwForgot from "./PwForgot";
 export default {
   name: "LogIn",
-  
+
   components: {
     Button,
-    Regist
+    Regist,
+    PwForgot,
   },
   data: () => ({
     accScope: false,
-    pwScope: false
+    pwScope: false,
   }),
   methods: {
     accEvent() {
-      this.pwScope=false
-      this.accScope=!this.accScope
+      this.pwScope = false;
+      this.accScope = !this.accScope;
     },
     pwEvent() {
-      this.accScope=false
-      this.pwScope=!this.pwScope
-    }
-  }
+      this.accScope = false;
+      this.pwScope = !this.pwScope;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-#main{
+#main {
   display: inline-block;
 }
-.conDesign{
+.conDesign {
   border-radius: 1em;
   background-color: #009a93;
   padding: 1em;
@@ -66,7 +71,7 @@ export default {
   height: 9em;
   font-size: 1.3em;
   letter-spacing: 0.1em;
-  transition: all .65s;
+  transition: all 0.65s;
 }
 
 #container {
@@ -80,7 +85,7 @@ export default {
   grid-auto-columns: 25% 40% 35%;
   grid-auto-rows: 15% 25% 25% 25% 10%;
 }
-#anmText{
+#anmText {
   grid-area: a;
   display: flex;
   align-self: center;
@@ -89,10 +94,10 @@ export default {
   justify-self: center;
 }
 
-#p1{
+#p1 {
   grid-area: p1;
 }
-#p2{
+#p2 {
   grid-area: p2;
 }
 #form {
@@ -105,78 +110,85 @@ export default {
   justify-content: center;
   margin-right: 1em;
 }
-#reg{
+#reg {
   display: flex;
-  align-items: flex-end;
-  justify-content: center;
+  align-self: flex-end;
+  justify-self: center;
   grid-area: reg;
   font-size: 60%;
+  width: 7em;
   color: white;
-  &:hover{
+  &:hover {
     color: black;
     cursor: pointer;
   }
 }
-#pwv{
+#pwv {
   display: flex;
-  align-items: flex-end;
-  justify-content: center;
+  align-self: flex-end;
+  justify-self: center;
   grid-area: pwv;
   font-size: 60%;
+  width: 13em;
   color: white;
-  &:hover{
+  &:hover {
     color: black;
     cursor: pointer;
   }
 }
-.canttouchme{
-    -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-     -khtml-user-select: none; /* Konqueror HTML */
-       -moz-user-select: none; /* Old versions of Firefox */
-        -ms-user-select: none; /* Internet Explorer/Edge */
-            user-select: none; /* Non-prefixed version, currently
+.canttouchme {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome, Edge, Opera and Firefox */
 }
 
 $color: #037c76;
-input{
+input {
   border: 1px solid #034d4a;
-  border-radius: .5em;
+  border-radius: 0.5em;
   background-color: #037c76;
   color: white;
   width: 16.2em;
   height: 1.7em;
   text-align: center;
-  font-size: .8em;
-  &:focus{
+  font-size: 0.8em;
+  &:focus {
     outline: none;
     background-color: darken(#037c76, 7%);
   }
 }
 
-form{
+form {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
 }
 
-p{
+p {
   display: flex;
   justify-content: flex-end;
   align-items: center;
 }
 
-@media screen and (max-width:768px){
-    .conDesign{
-        font-size: 1em;
-    }
+@media screen and (max-width: 768px) {
+  .conDesign {
+    font-size: 1em;
+  }
 }
 
-@media screen and (max-width:480px){
-    .conDesign{
-        font-size: .75em;
-    }
+@media screen and (max-width: 480px) {
+  .conDesign {
+    font-size: 0.75em;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 </style>
---
+++ src/components/screens/PwForgot.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="pwScreen" class="conDesign">
+            <p id="pwTitle">Passwort vergessen</p>
+            <p id="elem1">Email Adresse eingeben</p>
+        <form>
+            <input type="text">
+        </form>
+        <Button id="absPw" name="Absenden"/>
+    </div>
+</template>
+
+<script>
+import Button from "./../Button";
+export default {
+    components: {
+    Button
+  }
+}
+</script>
+
+<style>
+#pwScreen{
+    display: grid;
+    grid-template-areas: 
+        "t t"
+        "n i"
+        ". a";
+    grid-auto-columns: 25% 75%;
+    grid-auto-rows: 25% 50% 25%;
+    height: 6em;
+}
+#pwTitle{
+    grid-area: t;
+    display: flex;
+    align-self: center;
+    justify-self: center;
+}
+#elem1{
+    grid-area: n;
+    font-size: .6em;
+}
+form{
+    grid-area: i;
+}
+#absPw{
+    grid-area: a;
+    display: flex;
+    align-self: center;
+    justify-self: center;
+}
+</style>
