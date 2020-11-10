e1e4b03b38d3d6a4c95244338d805c839f7a5266
Selim <elasseli@googlemail.com>
2020-11-10 12:18:38 +0100

    Update_Git_Ignore_Fix

---
+++ src/components/screens/HomeComp/GridElement.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="items">{{props.item}}</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            items: [
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/screens/HomeComp/Grid_ui.vue
@@ -0,0 +1,67 @@
+<template>
+    <grid
+        :draggable="true"
+        :sortable="true"
+        :items="items"
+        :cellWidth="300"
+        :cellHeight="300"
+        >
+        <template slot="cell" slot-scope="props">
+            <div>{{props.item}}</div>
+        </template>
+    </grid>
+</template>
+
+<script>
+import Vue from 'vue'
+import Grid from 'vue-js-grid'
+Vue.use(Grid)
+export default {
+    components: {
+        
+    },
+    data () {
+        return {
+            items: [
+                'Programmierkurs 1',
+                'Programmierkrus 2',
+                'Einführung in Hallo',
+                'Was geht abologie',
+                'CoronaMorona',
+                'Privat',
+                'Privat',
+                'Privat',
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/theme.scss";
+.v-grid{
+    z-index: 5;  
+    width: 50%;
+    left: 17em;
+    //top: 3em;
+    //border: 1px solid black;
+
+}
+.v-grid-item-wrapper{
+    border: 1px solid black;
+    
+}
+
+.v-grid-item-wrapper div{
+    border: 1px black solid;
+    border-radius: 1em;
+    background-color: $home_menu_color;
+    box-shadow: 1px 1px 2px rgb(126, 126, 126);
+    box-sizing: border-box;
+    letter-spacing: 0.1em;
+    width: 250px;
+    height: 250px;
+    margin: 1em;
+    color: red;
+}
+</style>
