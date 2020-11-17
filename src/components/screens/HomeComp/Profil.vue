3811fd0b9d327254e39ae9b29c09b60f48e6e880
muhammad <robar96@hotmail.de>
2020-11-17 14:40:16 +0100

    Profil beendet

--- src/components/screens/HomeComp/Profil.vue
+++ src/components/screens/HomeComp/Profil.vue
@@ -18,10 +18,12 @@
             <input type="text" size="20" maxlength="20" v-model="username"  autocomplete="off" spellcheck="false" id="boxInputProfil"> 
             <input type="text" size="20" maxlength="20" v-model="firstname"  autocomplete="off" spellcheck="false">
             <input type="text" size="20" maxlength="20" v-model="lastname"  autocomplete="off" spellcheck="false">  
-            <input type="text" size="20" maxlength="20" v-model="email"  autocomplete="off" spellcheck="false">
+            <input type="text" size="20" maxlength="32" v-model="email"  autocomplete="off" spellcheck="false">
             <textarea :placeholder="hi" maxlength="64" v-model="bio" autocomplete="off" spellcheck="false"></textarea>
         </div>
+        <span @click="saveAll">
         <Button  id="updatePw" class="canttouchme" name="Save changes" />
+        </span>
         <div class="ueberschrift">Change Password</div>
         <div class="profilSetting" id="changePw">
             <p>Old Password</p>
@@ -47,10 +49,15 @@ export default {
     },
     data: () => ({
         username: "",
+        usernameold: "",
         firstname: "",
+        firstnameold: "",
         lastname: "",
+        lastnameold: "",
         email: "",
+        emailold: "",
         bio: "",
+        bioold: "",
         passwort: "",
         passwortOld: "",
         passwortNew: "",
@@ -59,16 +66,115 @@ export default {
     methods: {
         saveAll()
         {
-           /* var r=require("request");
+            var r=require("request");
             var txUrl = "http://localhost:7474/db/data/transaction/commit";
             function cypher(query,params,cb) 
             {
                 r.post({uri:txUrl,
                 json:{statements:[{statement:query,parameters:params}]}},
                 function(err,res) { cb(err,res.body)})
-            }*/
+            }
+            var change = this.checkNew2();
+            if(change.first)
+            {
+                var query="MATCH (b:Benutzer { benutzername: $benutzername }) SET b.firstname = $firstname RETURN b";
+                var params={benutzername: this.username, firstname: this.firstname};
+                var cb=function(err,data) 
+                {
+                    if(data.results[0].data.length!=0) 
+                    {
+                        alert("Firstname wurde erfolgreich verändert") //Grünes Häckchen mit Meldung 
+                        this.firstnameold = this.firstname;
+                    }
+                }.bind(this);
+                cypher(query,params,cb);
+            }
+            if(change.last)
+            {
+                var query2="MATCH (b:Benutzer { benutzername: $benutzername }) SET b.lastname = $lastname RETURN b";
+                var params2={benutzername: this.username, lastname: this.lastname};
+                var cb2=function(err,data) 
+                {
+                    if(data.results[0].data.length!=0) 
+                    {
+                        alert("Lastname wurde erfolgreich verändert") //Grünes Häckchen mit Meldung
+                        this.lastnameold = this.lastname; 
+                    }
+                }.bind(this);
+                cypher(query2,params2,cb2);
+            }
+            if(change.mail)
+            {
+                var query3="MATCH (b:Benutzer { benutzername: $benutzername }) SET b.email = $email RETURN b";
+                var params3={benutzername: this.username, email: this.email};
+                var cb3=function(err,data) 
+                {
+                    if(data.results[0].data.length!=0) 
+                    {
+                        alert("Email wurde erfolgreich verändert") //Grünes Häckchen mit Meldung 
+                        this.emailold = this.email;
+                    }
+                }.bind(this);
+                cypher(query3,params3,cb3);
+            }
+            if(change.bio)
+            {
+                var query4="MATCH (b:Benutzer { benutzername: $benutzername }) SET b.bio = $bio RETURN b";
+                var params4={benutzername: this.username, bio: this.bio};
+                var cb4=function(err,data) 
+                {
+                    if(data.results[0].data.length!=0) 
+                    {
+                        alert("Bio wurde erfolgreich verändert") //Grünes Häckchen mit Meldung 
+                        this.bioold = this.bio;
+                    }
+                }.bind(this);
+                cypher(query4,params4,cb4);
+            }
 
-
+        },
+        checkNew2() {
+            var name = /^[a-zA-Z]{6,}$/;
+            var mail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var change = {first: false, last:false, mail:false, bio:false}
+            if(this.firstnameold != this.firstname)
+            {
+                if(name.test(this.firstname))
+                {
+                    change.first = true;
+                }
+                else
+                {
+                    alert("Firstname Veränderung passt nicht zur Anforderung") //Rotes Häckchen mit Meldung 
+                }
+            }
+            if(this.lastnameold != this.lastname)
+            {
+                if(name.test(this.lastname))
+                { 
+                    change.last = true;
+                }
+                else
+                {
+                    alert("Lastname Veränderung passt nicht zur Anforderung") //Rotes Häckchen mit Meldung
+                }
+            }
+            if(this.emailold != this.email)
+            {
+                if(mail.test(this.email))
+                { 
+                    change.mail = true;
+                }
+                else
+                {
+                    alert("Email Veränderung Passt nicht zur Anforderung") //Rotes Häckchen mit Meldung
+                }
+            }
+            if(this.bio != this.bioold)
+            {
+                change.bio = true;
+            }
+            return change;
         },
         pwChange() {
             var r=require("request");
@@ -160,11 +266,11 @@ export default {
         var params={benutzername: this.name2};
         var cb=function(err,data) {
             if(data.results[0].data.length!=0) {
-                this.username = data.results[0].data[0].row[0].benutzername;
-                this.firstname = data.results[0].data[0].row[0].firstname;
-                this.lastname = data.results[0].data[0].row[0].lastname;
-                this.email = data.results[0].data[0].row[0].email;
-                this.bio = data.results[0].data[0].row[0].bio;
+               this.usernameold = this.username = data.results[0].data[0].row[0].benutzername;
+               this.firstnameold = this.firstname = data.results[0].data[0].row[0].firstname;
+               this.lastnameold = this.lastname = data.results[0].data[0].row[0].lastname;
+               this.emailold = this.email = data.results[0].data[0].row[0].email;
+               this.bioold = this.bio = data.results[0].data[0].row[0].bio;
                 this.passwort = data.results[0].data[0].row[0].passwort;
             }
         }.bind(this);
