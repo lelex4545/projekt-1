135b906b367eda90da9b525a23462d3910068672
Selim <elasseli@googlemail.com>
2020-12-03 11:25:13 +0100

    KategorienUpdate

--- src/components/screens/HomeComp/Wissensthema.vue
+++ src/components/screens/HomeComp/Wissensthema.vue
@@ -1,68 +1,101 @@
 <template>
-    <div>
-        <WissensthemaItem
-            v-for="item in items"
-            :key="item.id"
-            :item="item"
-            id="item"
-            @deleteItem="deleteItem"
-            @contextmenu="contextmenu"
-        />
-        <div v-if="items.length<maxItemCount" id="item" @click="addItem">
-            <p id="emptyItem">+</p>
-        </div>
-    </div>
+    <grid
+        id="grid"
+        :draggable="true"
+        :sortable="true"
+        :items="items"
+        :cellWidth="250"
+        :cellHeight="80"
+        @changeArray="changeArray"
+        @dragend="end"
+        @removeEvent="removeItem"
+        >
+        <template slot="cell" slot-scope="props">
+            <div
+                @contextmenu.prevent="() => {props.remove()}"
+                @dblclick="addItem"
+            >
+                {{items[props.sort].name}} {{items[props.sort].id}} {{props.index}}
+            </div>
+        </template>
+    </grid>
 </template>
 
 <script>
-import WissensthemaItem from "./WissensthemaItem";
+import Vue from 'vue'
+import Grid from 'vue-js-grid'
+Vue.use(Grid)
 export default {
-    name: "Wissensthema",
-    components: {
-        WissensthemaItem
+    data () {
+        return {
+            items: [
+                {id: '0', name: 'Null '},
+                {id: '1', name: 'Eins '},
+                {id: '2', name: 'Zwei '},
+                {id: '3', name: 'Drei '},
+                {id: '4', name: 'Vier '},
+            ]
+        }
+        
     },
-    data: () => ({
-        items: [],
-        maxItemCount: 7,
-    }),
     methods: {
-        addItem () {
-            this.items.push({id: this.items.length, name: "Neue Katogorie "})
+        addItem: function(){
+            this.items.push({id: this.items.length, name: 'Element' + this.items.length})
         },
-        deleteItem (item) {
-            const index = this.items.indexOf(item)
-            if(index > -1){
-                this.items.splice(index, 1)
-                for(var i=index; i < this.items.length; i++){
+        removeItem: function(props){
+            console.log(props)
+            if(props > -1)
+                //this.items = this.items.splice(props, 1);
+                this.items.splice(props, 1);
+            for(var i = 0; i<this.items.length; i++){
+                    this.items[i].id = i;
+            }
+        },
+        changeArray: function(list){
+            for(var i = 0; i<this.items.length; i++){
+                if(this.items[i] !== list[i].item){ 
+                    this.items[i] = list[i].item;
                     this.items[i].id = i;
                 }
             }
+            this.$emit("updateProps",this.items.length)
+            //document.getElementById('grid').item=this.items
         },
-        contextmenu (item)
-        {
-            alert(item.id)
+        end: function(){
+            this.items.push("test")
+            this.items.pop()
+        },
+        showProp: function(props){
+            console.log(props.item.name + " " + props.item.id)
         }
     }
-};
+}
 </script>
 
 <style lang="scss">
 @import "@/assets/theme.scss";
-#item{
+.v-grid-item-wrapper{
     display: flex;
-    align-items: center;
-    justify-content: space-around;
-    //justify-items: center;
-    background-color: $div_color;
-    box-shadow: 1px 1px 2px rgb(126, 126, 126);
-    border-radius: 0.5em;
-    width: 14em;
-    height: 4em;
-    margin-top: 1em;
+    justify-items: space-around;
+    margin: 1em;
 }
-#emptyItem{
-    color: $add_new_wissensnetz;
-    margin: auto;
-    font-size: 2.5em;
+.v-grid-item-wrapper div{
+    border: 1px black solid;
+    border-radius: 1rem;
+    background-color: #384b5e;
+    box-shadow: 1px 1px 2px rgb(126, 126, 126);
+    box-sizing: border-box;
+    
+    letter-spacing: 0.1em;
+    width: 250px;
+    height: 70px;
+    color: white;
+}
+#addNewItem{
+    display: flex;
+}
+#addNewItem p {
+    font-size: 2em;
+    margin: auto !important;
 }
 </style>
