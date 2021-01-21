6d5ae7ae6f721757eca69541a2e6b1e44916fa64
Selim <elasseli@googlemail.com>
2021-01-21 16:57:56 +0100

    APP
    App.vue_CleanUp

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,14 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: center; 
+  color: #ffffff;
+  height: 100%;
+  width: 100%;
+  margin: 0em;
 }
 
 #nav {
@@ -35,30 +38,6 @@
       color: #42b983;
     }
   }
-}
-
-#buttonContainer{
-  margin: 10em;
-}
-#myeditor{
-  margin-left: 400px;
-  margin-top: 100px;
-  border-radius: 30px;
-  padding: 50px;
-  background-color: white;
-  color: black;
-  width: 1000px;
-  text-align: left;
-}
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center; 
-  color: #ffffff;
-  height: 100%;
-  width: 100%;
-  margin: 0em;
 }
 html{
   margin: 0;
