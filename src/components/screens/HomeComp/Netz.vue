e61aab4d717c1ed65b66deda19e913cee547e505
muhammad <robar96@hotmail.de>
2021-03-15 21:34:26 +0100

    KOMMENTARE Netz

--- src/components/screens/HomeComp/Netz.vue
+++ src/components/screens/HomeComp/Netz.vue
@@ -43,21 +43,14 @@ export default {
             options: [{name: 'Name ändern'},{name: 'Löschen'}],
             click: (args) => {
                 let clickedItem = args.actualObject
-                if(args.button === 'Left' && clickedItem instanceof Object && clickedItem.constructor.name === 'Node'){ //Überprüfe, ob ein Node geklickt wurde
+                if(args.button === 'Left' && clickedItem instanceof Object && clickedItem.constructor.name === 'Node'){ //Überprüfe, ob ein Node mit der linken Maustaste geklickt wurde
                     this.saveInstance();
                 }
                 if(args.button === 'Right') {
-                    this.itemContextMenu(args)
-                    /*
-                    if(clickedItem instanceof Object && clickedItem.constructor.name === 'Node'){
-                        
-                    }
-                    */
+                    this.itemContextMenu(args) 
                 }
             },
-            /*positionChange: () =>{
-                console.log("DRAG")            
-            },*/
+            
             
             //--------------------------------------------
             scrollSettings: {
@@ -78,21 +71,15 @@ export default {
             pageSettings:{
                 boundaryConstraints: 'Diagram',
                 background:{
-                    /*color: 'grey',*/
+                 
                 },
-                /*width: 1640,
-                height: 1401,*/
             },
 
-            /*directives: {
-                'dragscroll': dragscroll
-            },*/
+            
             snapSettings: {
                 horizontalGridlines: {
-                    //lineColor: 'blue',
                 },
                 verticalGridlines: {
-                    //lineColor: 'blue',
                 }
             },
             selectedItems: {
@@ -103,8 +90,7 @@ export default {
     mounted(){
             let diagramInstance;
             let diagramObj = document.getElementById("diagram")
-            //console.log(this.$el)
-            diagramInstance = diagramObj.ej2_instances[0]; //HIER KOMMT EIN FEHLER!!! ej2_instance == null
+            diagramInstance = diagramObj.ej2_instances[0];
 
             diagramInstance.bringToFront();
             diagramInstance.dataBind();
@@ -120,50 +106,53 @@ export default {
 
             var cb=function(err,data) 
             {
-                console.log(data);
                 
                 this.netzId = data.results[0].data[0].meta[0].id;
+                //Event auslösen, um die NetzID zu übertragen
                 this.$emit('sendNetzId', this.netzId);
-                data.results[0].data[0].row[0].serialisierung
-
+                //Serialisierung: Diagram laden
                 if(data.results[0].data[0].row[0].serialisierung != "")
                     diagramInstance.loadDiagram(data.results[0].data[0].row[0].serialisierung)
-
+                //Hilfspuffer laden, um hinzugefügte Links(in Texteditor) bildlich darzustellen(Konnektoren)
                 if(data.results[0].data[0].row[0].puffer != ""){
                     var tmp = JSON.parse(data.results[0].data[0].row[0].puffer);
                     this.puffer = tmp;
-                    console.log(this.puffer)
+                    //Daten der gespeicherten Links in Konnektoren darstellen und in die Instanz hinzufügen
                     for(var i = 0;i<this.puffer.length;i++) {
                         this.connectors = {id: `${this.puffer[i].knoten1}_${this.puffer[i].knoten2}`,sourceID: this.puffer[i].knoten1,targetID: this.puffer[i].knoten2,targetDecorator: {shape: 'Custom'}, constraints: ConnectorConstraints.Default & ~ConnectorConstraints.Select, style: {strokeColor: '#7CDE7A', fill: '#7CDE7A', strokeWidth: 2}, }
-                        diagramInstance.add(this.connectors)     //Link Connector hinzufügen
+                        diagramInstance.add(this.connectors)
                     }
+                    //Speichern des Diagrams
                     this.saveInstance();
                 }
+                //Hilfspuffer laden, um gelöschte Links(in Texteditor) bildlich zu entfernen(Konnektoren)
                 if(data.results[0].data[0].row[0].puffer2 != ""){
                     var tmp2 = JSON.parse(data.results[0].data[0].row[0].puffer2);
                     this.puffer2 = tmp2;
                     console.log(this.puffer2)
+                    //Daten der gespeicherten Links in Konnektoren darstellen und aus der Instanz zu entfernen
                     for(var j = 0;j<this.puffer2.length;j++) {
                         this.connectors = {id: `${this.puffer2[j].knoten1}_${this.puffer2[j].knoten2}`,sourceID: this.puffer2[j].knoten1,targetID: this.puffer2[j].knoten2,targetDecorator: {shape: 'Custom'}, constraints: ConnectorConstraints.Default & ~ConnectorConstraints.Select, style: {strokeColor: '#7CDE7A', fill: '#7CDE7A', strokeWidth: 2}, }
                         diagramInstance.remove(this.connectors)
                         //Wir wollen einfach umbringen
                     }
+                    //Speichern des Diagrams
                     this.saveInstance();
                 }
+                //Event auslösen, um die existierenden Knoten zu senden
                 this.$emit('sendExistingNodes', diagramInstance.nodes);
-
+                //Datenbankabfrage, um die Puffer zu leeren
                 var query2="MATCH(k:Wissensnetz)-[r:beinhaltet]->(n:Netz) WHERE id(k)=$id SET n.puffer='', n.puffer2='' RETURN n"
                 var params2={id: this.item.id}
                 cypher(query2,params2,cb2)
             }.bind(this)
                 var cb2=function(err,data) 
             {
-                diagramInstance.connectors.forEach(function(element){
-                    console.log(element)
-                });
                 data;
             }.bind(this)
+            // Durch die Cookies das aktuell besuchte Wissensnetz identifizieren
             this.item =this.$cookies.get("item");
+            //Datenbankabfrage um die Daten des aktuellen Wissensnetz zu bekommen
             var query="MATCH(k:Wissensnetz)-[r:beinhaltet]->(n:Netz) WHERE id(k)=$id RETURN n"
             var params={id: this.item.id};
             cypher(query,params,cb);
@@ -172,7 +161,7 @@ export default {
     },
     watch:{
         knotenName: function(){
-
+            //Erstellen eines Knotens
             var serialisierung;
             var r=require("request");
             var txUrl = "http://localhost:7474/db/data/transaction/commit";
@@ -184,7 +173,7 @@ export default {
             var cb=function(err,data) 
             {
                 data;
-
+                //Erstellen eines Knotens in der Datenbank
                 var query2="CREATE(n:Text {array:'', htmlString:'', knotenId:$knotenId, counter:-1, deleteLinks:'[]'}) RETURN n"
                 var params2={knotenId: this.knotenName};
                 cypher(query2,params2,cb2)
@@ -193,6 +182,7 @@ export default {
 
             var cb2=function(err,data) 
             {
+                //Erstellen einer Verbindung zwischen den Datenbankknoten
                 var query3="MATCH (a:Netz),(b:Text) WHERE id(a)=$id AND id(b)=$id2 CREATE (a)-[r:besitzt]->(b) RETURN b"
                 var params3={id: this.netzId, id2: data.results[0].data[0].meta[0].id}
                 cypher(query3,params3,cb3);
@@ -206,20 +196,14 @@ export default {
             let diagramInstance;
             let diagramObj = document.getElementById("diagram");
             diagramInstance = diagramObj.ej2_instances[0];
-        
+            //Erstellter Knoten in Instanz hinzufügen
             this.nodes = {id: this.knotenName, offsetX: 600,offsetY: 300,width: 100,height: 100,style: {fill: '#6964FF',strokeColor: '#8D8AFF'},annotations: [{content: this.knotenName}],shape: {type: 'Basic',shape: 'Ellipse',cornerRadius: 10}, }
             diagramInstance.add(this.nodes)
-            //vorhandene knoten aus diagram benötigt
+            //Vorhandene Knoten aus Diagram benötigt
             this.$emit('sendExistingNodes', diagramInstance.nodes);
 
-            //--------------------------------------Connector
-
-            /*for(var j = 0; j < this.connectorNodes.length; j++){
-                var targetid = this.connectorNodes[j].substring(0, this.connectorNodes[j].length-1);
-                this.connectors = {id: `${this.knotenName} ${j}`,sourceID: this.knotenName,targetID: targetid,targetDecorator: {shape: 'Custom'}, constraints: ConnectorConstraints.Default & ~ConnectorConstraints.Select, style: {strokeColor: '#6BA5D7', fill: '#6BA5D7', strokeWidth: 2}, }             
-                diagramInstance.add(this.connectors)
-            }*/
-            //  Datenbankanbindung/Serialisierung
+            
+            //Speichern des Diagrams
             serialisierung = diagramInstance.saveDiagram();
             var query="MATCH(k:Wissensnetz)-[r:beinhaltet]->(n:Netz) WHERE id(k)=$id SET n.serialisierung=$serialisierung RETURN n"
             var params={id: this.item.id, serialisierung: serialisierung};
@@ -238,24 +222,7 @@ export default {
             var tmp2D = [];
             var cb=function(err,data) 
             {
-                console.log(data);
-                /*for(var i = 0; i < data.results[0].data.length; i++){
-                    var knotenName = data.results[0].data[i].row[0].knotenId;
-                    if(data.results[0].data[i].row[0].deleteLinks === ''){
-                        tmp2D[i]= {name: knotenName, array: []}
-                    }
-                    else{
-                        tmp2D[i]= {name: knotenName, array: JSON.parse(data.results[0].data[i].row[0].deleteLinks)}
-                    }
-                }
-                for(var j = 0; j < tmp2D.length;j++){
-                    tmp2D[j].array.push(node.id)
-                }
-                for(var k = 0; k < tmp2D.length; k++){
-                var query2="MATCH(n:Netz)-[r:besitzt]->(t:Text) WHERE id(n)=$id SET t.deleteLinks=$array RETURN t"
-                var params2={id: this.netzId, array: JSON.stringify(tmp2D[k].array)};
-                cypher(query2,params2,cb2)
-                }*/
+                //Daten aller Wissensknoten abfangen und zur Berarbeitung sicherstellen
                 for(var i = 0; i < data.results[0].data.length; i++){
                     var knotenName = data.results[0].data[i].row[0].knotenId;
                     var tmpA = [];
@@ -268,6 +235,7 @@ export default {
 
                     tmp2D[i] = {name: knotenName, array: tmpA, htmlString: tmpB}
                 }
+                //Entfernen aller möglichen Assoziation  mit dem gelöschten Knoten
                 for(var j = 0; j < tmp2D.length; j++){
                     var array = tmp2D[j].array;
                     var htmlString = tmp2D[j].htmlString;
@@ -276,22 +244,18 @@ export default {
                         if(array[k].knotenName === node.id){
                             deleteIndex.push(k) //muss array ändern
                             var element = $('<div></div>').html(htmlString);
-                            console.log(element.html())
                             element.find("#"+array[k].id).contents().unwrap();
-                            console.log(element.html())
                             htmlString = element.html()
-                            console.log(htmlString)
                             tmp2D[j].htmlString = htmlString;
                         }
                     }
-                    console.log(array)
+
                     for(var l = deleteIndex.length-1; l>=0;l--){
                         array.splice(deleteIndex[l],1)
                     }
-                    //console.log(array)
                     tmp2D[j].array = array
                 }
-
+                //Datenbankabfrage: Bearbeitende Daten speichern
                 for(var x = 0; x < tmp2D.length; x++){
                 var query2="MATCH(n:Netz)-[r:besitzt]->(t:Text) WHERE id(n)=$id AND t.knotenId=$id2 SET t.array=$array, t.htmlString=$htmlString RETURN t"
                 var params2={id: this.netzId, id2: tmp2D[x].name, array: JSON.stringify(tmp2D[x].array), htmlString: tmp2D[x].htmlString};
@@ -305,11 +269,12 @@ export default {
             var cb3=function(err,data) 
             {
                 data
-            var query4="MATCH(n:Text) WHERE id(n)=$id DELETE n"
-            var params4={id: data.results[0].data[0].meta[0].id};
+                //Datenbankabfrage: Löschen des Textknotens(Wissensnetzknoten)
+                var query4="MATCH(n:Text) WHERE id(n)=$id DELETE n"
+                var params4={id: data.results[0].data[0].meta[0].id};
             cypher(query4, params4, cb4)
             }.bind(this)
-            var cb4=function(err,data) 
+                var cb4=function(err,data) 
             {
                 data
             }.bind(this)
@@ -319,22 +284,20 @@ export default {
             diagramInstance.connectors.forEach(function(element){
                     console.log(element)
                 });
-            // removes associated Connectors
+            // Alle assoziernde Knoten löschen
             
             diagramInstance.connectors.forEach(function(element){
                 if(element.sourceID == node.id || element.targetID == node.id){
                     diagramInstance.remove(element);
                 }
             });
-            diagramInstance.connectors.forEach(function(element){
-                    console.log(element)
-                });
-            // Removes from the Diagram
+            // Knoten löschen aus dem Diagram
             diagramInstance.remove(node);
-
             this.saveInstance();
+            //Datenbankabfrage: Alle Knoten im Wissensnetz zurückgeben
             var query="MATCH(n:Netz)-[r:besitzt]->(t:Text) WHERE id(n)=$id RETURN t"
             var params={id: this.netzId};
+            //Datenbankabfrage: Verbindung zwischen Netz und Text(dem gelöschten Knoten) entfernen
             var query3="MATCH(n:Netz)-[r:besitzt]->(t:Text) WHERE id(n)=$id AND t.knotenId=$id2 DELETE r RETURN t"
             var params3={id: this.netzId, id2: node.id};
             cypher(query,params,cb);
@@ -373,9 +336,9 @@ export default {
             function(err,res) { cb(err,res.body)})}
 
             var cb=function(err,data) {
-                    console.log(data.results[0].data[0].meta[0].id);
+                data;
             }.bind(this)
-
+            //Datenbankabfrage, um die Diagraminstanz zu speichern
             serialisierung = diagramInstance.saveDiagram();
             var query="MATCH(k:Wissensnetz)-[r:beinhaltet]->(n:Netz) WHERE id(k)=$id SET n.serialisierung=$serialisierung RETURN n"
             var params={id: this.item.id, serialisierung: serialisierung};
@@ -387,12 +350,6 @@ export default {
             console.log(connectorKnoten)
             this.puffer.push(connectorKnoten);
             this.savePuffer();
-            /*let diagramInstance;
-            let diagramObj = document.getElementById("diagram")
-            diagramInstance = diagramObj.ej2_instances[0];
-            console.log('myMethod called', connectorKnoten);
-            this.connectors = {id: `${connectorKnoten.knoten1}_${connectorKnoten.knoten2}`,sourceID: connectorKnoten.knoten1,targetID: connectorKnoten.knoten2,targetDecorator: {shape: 'Custom'}, constraints: ConnectorConstraints.Default & ~ConnectorConstraints.Select, style: {strokeColor: '#6BA5D7', fill: '#6BA5D7', strokeWidth: 2}, }
-            diagramInstance.add(this.connectors)*/
         },
         savePuffer(){
             var r=require("request");
